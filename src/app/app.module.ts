95784499b94f71d2ef6a4a1e8ec4c695a609f873
nidia <nidiacmh@gmail.com>
2020-12-30 16:39:27 -0600

    demos de propiedades de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { CssComponent } from './components/css/css.component';
+import { ClasesComponent } from './components/clases/clases.component';
+import { ResaltadoDirective } from './directives/resaltado.directive';
+import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgStyleComponent,
+    CssComponent,
+    ClasesComponent,
+    ResaltadoDirective,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule,
