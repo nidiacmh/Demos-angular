f9f294fe378eb3cdc8c89358aacd93e1e015699c
nidia <nidiacmh@gmail.com>
2021-01-05 11:53:10 -0600

    Rutas hijas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import {RouterModule, Routes} from '@angular/router';
 
 import{HomeComponent} from './components/home/home.component';
 import{UsuarioComponent} from './components/usuario/usuario.component';
+import {USUARIOS_ROUTES} from './components/usuario/uduario.routes';
 
 const APP_ROUTES: Routes = [
   {path: 'home', component:HomeComponent},
-  {path: 'usuario', component:UsuarioComponent},
+  {path: 'usuario/:id', component:UsuarioComponent,
+    children:USUARIOS_ROUTES,
+  },
   {path: '**', pathMatch:'full', redirectTo:'home'}
 ];
 
